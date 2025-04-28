a8ecd67d3b95a5c409e9a496b3305de684891334
ivansteb <steblerivan@gmail.com>
2025-04-28 01:27:44 -0300

    refactor useStore reducer for improved readability and consistency

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,107 +1,98 @@
-import { useReducer } from 'react';
-import { Action, FromLanguage, Language, type State } from '../types';
-import { AUTO_LANGUAGE } from '../constants';
+import { useReducer } from "react";
+import { Action, FromLanguage, Language, type State } from "../types";
+import { AUTO_LANGUAGE } from "../constants";
 
 const initialState: State = {
-    fromLanguage: 'auto',
-    toLanguage: 'en',
-    fromText: '',
-    result: '',
-    loading: false
-  }
-  
-  // useReducer: se le pasan initialState y reducer
-  // el reducer recibe la action por dispatch y genera nuevo state
-  function reducer (state: State, action: Action) {
-    const { type } = action;
-  
-    if (type === 'INTERCHANGE_LANGUAGES') {
+  fromLanguage: "auto",
+  toLanguage: "en",
+  fromText: "",
+  result: "",
+  loading: false,
+};
 
-        if (state.fromLanguage === AUTO_LANGUAGE) return state;
+// useReducer: se le pasan initialState y reducer
+// el reducer recibe la action por dispatch y genera nuevo state
+function reducer(state: State, action: Action) {
+  const { type } = action;
 
-        return {
-        ...state,
-        fromLanguage: state.toLanguage,
-        toLanguage: state.fromLanguage
-        }
-    }
-  
-    if (type === 'SET_FROM_LANGUAGE') {
-        return {
-        ...state,
-        fromLanguage: action.payload
-        }
-    }
-  
-    if (type === 'SET_TO_LANGUAGE') {
-        return {
-        ...state,
-        toLanguage: action.payload
-        }
-    }
-  
-    if (type === 'SET_FROM_TEXT') {
-        return {
-        ...state,
-        loading: true,
-        fromText: action.payload,
-        result: ''
-        }
-    }
-  
-    if (type === 'SET_RESULT') {
-        return {
-        ...state,
-        loading: false,
-        result: action.payload
-        }
-    }
-  
-    return state;
-  }
-
-  export function useStore() {
-    const [{
-        fromLanguage,
-        toLanguage,
-        fromText,
-        result,
-        loading
-    }, dispatch] = useReducer(reducer, initialState);
-
-    const interchangeLanguages = () => {
-        dispatch({ type: 'INTERCHANGE_LANGUAGES' });
-    }
-
-    const setFromLanguage = (payload: FromLanguage) => {
-        dispatch({ type: 'SET_FROM_LANGUAGE', payload });
-    }
-
-    const setToLanguage = (payload: Language) => {
-        dispatch({ type: 'SET_TO_LANGUAGE', payload });
-    }
-
-    const setFromText = (payload: string) => {
-        dispatch({ type: 'SET_FROM_TEXT', payload });
-    }
-
-    const setResult = (payload: string) => {
-        dispatch({ type: 'SET_RESULT', payload });
-    }
-
-
-
+  if (type === "INTERCHANGE_LANGUAGES") {
+    if (state.fromLanguage === AUTO_LANGUAGE) return state;
 
     return {
-        fromLanguage,
-        toLanguage,
-        fromText,
-        result,
-        loading,
-        interchangeLanguages,
-        setFromLanguage,
-        setToLanguage,
-        setFromText,
-        setResult
-    }
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage,
+    };
   }
+
+  if (type === "SET_FROM_LANGUAGE") {
+    return {
+      ...state,
+      fromLanguage: action.payload,
+    };
+  }
+
+  if (type === "SET_TO_LANGUAGE") {
+    return {
+      ...state,
+      toLanguage: action.payload,
+    };
+  }
+
+  if (type === "SET_FROM_TEXT") {
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+      result: "",
+    };
+  }
+
+  if (type === "SET_RESULT") {
+    return {
+      ...state,
+      loading: false,
+      result: action.payload,
+    };
+  }
+
+  return state;
+}
+
+export function useStore() {
+  const [{ fromLanguage, toLanguage, fromText, result, loading }, dispatch] =
+    useReducer(reducer, initialState);
+
+  const interchangeLanguages = () => {
+    dispatch({ type: "INTERCHANGE_LANGUAGES" });
+  };
+
+  const setFromLanguage = (payload: FromLanguage) => {
+    dispatch({ type: "SET_FROM_LANGUAGE", payload });
+  };
+
+  const setToLanguage = (payload: Language) => {
+    dispatch({ type: "SET_TO_LANGUAGE", payload });
+  };
+
+  const setFromText = (payload: string) => {
+    dispatch({ type: "SET_FROM_TEXT", payload });
+  };
+
+  const setResult = (payload: string) => {
+    dispatch({ type: "SET_RESULT", payload });
+  };
+
+  return {
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage,
+    setFromText,
+    setResult,
+  };
+}
